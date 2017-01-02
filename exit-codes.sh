5211a68cc96534c20fb1443cdbe677d3c7855650
Willem Oosting <woosting@users.noreply.github.com>
2017-01-02 20:02:15 +0100

    Update exit-codes.sh

--- exit-codes.sh
+++ exit-codes.sh
@@ -1,28 +1,28 @@
 
 #! /bin/bash
 
-echo -e " "
+echo -e ""
 echo -e "Successful execution"
 echo -e "====================="
 echo "hello world"
 # Exit status returns 0, because the above command is a success.
 echo "Exit status" $?
 
-echo -e " "
+echo -e ""
 echo -e "Incorrect usage"
 echo -e "====================="
 ls --option
 # Incorrect usage, so exit status will be 2.
 echo "Exit status" $?
 
-echo -e " "
+echo -e ""
 echo -e "Command Not found"
 echo -e "====================="
 bashscript
 # Exit status returns 127, because bashscript command not found
 echo "Exit status" $?
 
-echo -e " "
+echo -e ""
 echo -e "Command is not an executable"
 echo -e "============================="
 ls -l execution.sh
